b5cb24dda2868a4c2c3a5beeabf11a6a01582197
Eric Hwang <eric_hwang@college.harvard.edu>
2024-11-30 23:42:04 -0600

    first implementation of both summarization strategies

---
+++ simplify-privacy/src/helpers/summarization.js
@@ -0,0 +1,87 @@
+import { OpenAI } from "@langchain/openai";
+import { loadSummarizationChain } from "langchain/chains";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+const secrets = require('../env.json');
+const OPENAI_API_KEY = secrets.OPENAI_API_KEY;
+const OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
+
+
+
+// Naive summarization strategy
+async function stuff(text) {
+
+  // Request payload
+  const data = {
+    model: "gpt-4", 
+    messages: [
+      { role: "system", content: "You are a helpful assistant that summarizes privacy policies." },
+      {
+        role: "user",
+        content: `Please concisely summarize the following privacy policy: ${text}`,
+      },
+    ],
+    temperature: 0.7,
+  };
+
+  // Make the POST request
+  try {
+    const response = await fetch(OPENAI_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify(data),
+    });
+
+    console.log(response);
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const completion = await response.json();
+    console.log(completion);
+    console.log("Completion response:", completion.choices[0].message.content);
+    return {"status": 200, "summary": completion.choices[0].message.content}
+    
+  } catch (error) {
+    console.error("Error:", error);
+    return  {"status": 400, "error_msg": "PLACEHOLDER"}
+
+  }
+}
+
+// MapReduce (aka chunking) summarization strategy
+async function mapReduce(text) {
+  const model = new OpenAI({ 
+    temperature: 0.7,
+    apiKey: OPENAI_API_KEY,
+  });
+  const textSplitter = new RecursiveCharacterTextSplitter({ 
+    chunkSize: 1000,
+    // chunkOverlap: 20,
+  });
+  try {
+    const docs = await textSplitter.createDocuments([text]);
+    console.log(docs)
+    const chain = loadSummarizationChain(model, { 
+      type: "map_reduce",
+      returnIntermediateSteps: true,
+    });
+
+    const res = await chain.invoke({
+      input_documents: docs,
+    });
+
+    console.log({ res });
+    console.log("intermediary steps:", res.intermediateSteps)
+    return {"status": 200, "summary": res.text}
+  } catch (error) {
+    console.error(error);
+    return {"status": 400, "error_msg": "PLACEHOLDER"}
+  }
+}
+
+export { stuff, mapReduce }
--- simplify-privacy/src/pages/Home.js
+++ simplify-privacy/src/pages/Home.js
@@ -1,18 +1,27 @@
 import { H4 } from '../components/ui/typography';
 import Button from '@mui/material/Button';
+import OpenAI from "openai";
+import { stuff, mapReduce } from "../helpers/summarization";
+
 
 
 function Home({setLoading, setSummary, setError, setScores}) {
+  // const secrets = require('../env.json');
 
-  const testSummary = `Data Collection: Personal details (e.g., name, email) and non-personal data (e.g., device info) are collected for services and improvements.
-Usage: Data is used for services, transactions, communication, and analytics.
-Sharing: Shared only with trusted third parties or for legal compliance.
-Security: Measures are in place to protect your information.
-Cookies: Used for functionality and personalization; users can manage settings.
-Third Parties: Not responsible for external sites linked on the platform.
-Your Rights: Access, edit, or delete your data; opt-out of marketing.
-Updates: Policy changes take effect when posted; check regularly.
-Contact: Reach out for questions or concerns.`;
+  // const openai = new OpenAI();
+  // const OPENAI_API_KEY = secrets.OPENAI_API_KEY;
+  
+
+  const testText = 
+    `Data Collection: Personal details (e.g., name, email) and non-personal data (e.g., device info) are collected for services and improvements.
+    Usage: Data is used for services, transactions, communication, and analytics.
+    Sharing: Shared only with trusted third parties or for legal compliance.
+    Security: Measures are in place to protect your information.
+    Cookies: Used for functionality and personalization; users can manage settings.
+    Third Parties: Not responsible for external sites linked on the platform.
+    Your Rights: Access, edit, or delete your data; opt-out of marketing.
+    Updates: Policy changes take effect when posted; check regularly.
+    Contact: Reach out for questions or concerns.`;
   const testScores = {
     "Transparency": 4,
     "Data Sharing": 3,
@@ -20,21 +29,23 @@ Contact: Reach out for questions or concerns.`;
     "Past Behavior": 5,
   };
 
-  const handleClick = () => {
+  const handleClick = async () => {
     setLoading(true);
-    
+    const result = await mapReduce(testText);
+    // const result = await stuff();
+    if (result.status === 200) {
+      setSummary(result.summary);
+      console.log(result.summary);
+      setScores(testScores);
+      setLoading(false);
+    }
+    else {
+      setError(true);
+    }
+
     // TODO: Add LLM/Score logic
     // TODO: If domain already cached, fetch cached result. 
     // TODO: If domain not cached, get new result and store in cache 
-
-    // Testing logic with 3 second delay for now
-    setTimeout(() => {
-      setSummary(testSummary);
-      setScores(testScores);
-      setLoading(false)
-    }, 3000)
-
-    // setLoading(false);
   }
  
   return (
